f21bf80113c81dc69a81438c0d61bb40fdda613b
emiliaelizabeth <emiliaelizabeth@gmail.com>
2023-11-17 18:36:43 -0300

    Crud Memoria com Horse.Jhonson

--- prjAPI_dhp.dpr
+++ prjAPI_dhp.dpr
@@ -4,18 +4,42 @@ program prjAPI_dhp;
 
 {$R *.res}
 
-uses Horse;
+uses Horse, System.JSON, Horse.Jhonson, System.SysUtils;
 
 var
   App: THorse;
+  Users: TJSONArray;
 
 begin
 //  App := THorse.Create(9000);
+  Users := TJSONArray.Create;
+//  App.Use(Jhonson);
+  THorse.Use(Jhonson);
 
-  THorse.Get('/ping',
+  THorse.Get('/users',
     procedure(Req: THorseRequest; Res: THorseResponse)
     begin
-      Res.Send('pong');
+      Res.Send<TJSONAncestor>(Users.Clone);
+    end);
+
+  THorse.Post('/user',
+    procedure(Req: THorseRequest; Res: THorseResponse)
+    var
+        User: TJSONObject;
+    begin
+      User := Req.Body<TJSONObject>.Clone as TJSONObject;
+      Users.AddElement(User);
+      Res.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
+    end);
+
+  THorse.Delete('/user/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse)
+    var
+        id: Integer;
+    begin
+      id := Req.Params.Items['id'].ToInteger;
+      Users.Remove(Pred(id)).Free;
+      Res.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.NoContent);
     end);
 
   THorse.Listen(9000);
